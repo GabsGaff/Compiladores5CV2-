20c3b3c5163a4a2feaf5b160b1202880560813a1
GabsGaff <127435206+GabsGaff@users.noreply.github.com>
2023-03-30 13:04:53 -0600

    Update scanner tokens y reservadas
    Existia un comentario que no permitio realizar la carga del comentario y se volvio a cargar

--- AnalizadorLexico/Scanner.java
+++ AnalizadorLexico/Scanner.java
@@ -1,82 +1,272 @@
-package AnalizadorLexico;
-
-import java.util.ArrayList;
+import java.io.IOException;
+import java.io.Reader;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class Scanner {
-
-    private final String source;
-
-    private final List<Token> tokens = new ArrayList<>();
-
-    private int linea = 1;
-
-    private static final Map<String, TipoToken> palabrasReservadas;
-    static {
-        palabrasReservadas = new HashMap<>();
-        palabrasReservadas.put("y", TipoToken.Y);
-        palabrasReservadas.put("clase", TipoToken.CLASE);
-        /*palabrasReservadas.put("ademas", );
-        palabrasReservadas.put("falso", );
-        palabrasReservadas.put("para", );
-        palabrasReservadas.put("fun", ); //definir funciones
-        palabrasReservadas.put("si", );
-        palabrasReservadas.put("nulo", );
-        palabrasReservadas.put("o", );
-        palabrasReservadas.put("imprimir", );
-        palabrasReservadas.put("retornar", );
-        palabrasReservadas.put("super", );
-        palabrasReservadas.put("este", );
-        palabrasReservadas.put("verdadero", );
-        palabrasReservadas.put("var", ); //definir variables
-        palabrasReservadas.put("mientras", );*/
+    
+    private Reader input;
+    private int currentChar;
+    private Map<String, TipoToken> palabrasReservadas;
+    
+    public Scanner(Reader input) {
+        this.input = input;
+        this.currentChar = -1;
+        this.palabrasReservadas = new HashMap<>();
+        this.palabrasReservadas.put("y", TipoToken.Y);
+        this.palabrasReservadas.put("si", TipoToken.IF);
+        this.palabrasReservadas.put("mas", TipoToken.ELSE);
+        this.palabrasReservadas.put("mientras", TipoToken.WHILE);
+        this.palabrasReservadas.put("para", TipoToken.FOR);
+        this.palabrasReservadas.put("hacer", TipoToken.DO);
+        this.palabrasReservadas.put("interruptor", TipoToken.SWITCH);
+        this.palabrasReservadas.put("caso", TipoToken.CASE);
+        this.palabrasReservadas.put("predeterminado", TipoToken.DEFAULT);
+        this.palabrasReservadas.put("romper", TipoToken.BREAK);
+        this.palabrasReservadas.put("continuar", TipoToken.CONTINUE);
+        this.palabrasReservadas.put("regresar", TipoToken.RETURN);
+        this.palabrasReservadas.put("probar", TipoToken.TRY);
+        this.palabrasReservadas.put("atrapar", TipoToken.CATCH);
+        this.palabrasReservadas.put("tirar", TipoToken.THROW);
+        this.palabrasReservadas.put("finalmente", TipoToken.FINALLY);
+        this.palabrasReservadas.put("publico", TipoToken.PUBLIC);
+        this.palabrasReservadas.put("privado", TipoToken.PRIVATE);
+        this.palabrasReservadas.put("protegido", TipoToken.PROTECTED);
+        this.palabrasReservadas.put("clase", TipoToken.CLASS);
+        this.palabrasReservadas.put("interface", TipoToken.INTERFACE);
+        this.palabrasReservadas.put("enum", TipoToken.ENUM);
+        this.palabrasReservadas.put("extiende", TipoToken.EXTENDS);
+        this.palabrasReservadas.put("implementar", TipoToken.IMPLEMENTS);
+        this.palabrasReservadas.put("paquete", TipoToken.PACKAGE);
+        this.palabrasReservadas.put("importar", TipoToken.IMPORT);
+        this.palabrasReservadas.put("estatico", TipoToken.STATIC);
+        this.palabrasReservadas.put("final", TipoToken.FINAL);
+        this.palabrasReservadas.put("abstracto", TipoToken.ABSTRACT);
+        this.palabrasReservadas.put("sincronizado", TipoToken.SYNCHRONIZED);
+        this.palabrasReservadas.put("volatil", TipoToken.VOLATILE);
+        this.palabrasReservadas.put("transient", TipoToken.TRANSIENT);
+        this.palabrasReservadas.put("nativo", TipoToken.NATIVE);
+        this.palabrasReservadas.put("super", TipoToken.SUPER);
+        this.palabrasReservadas.put("este", TipoToken.THIS);
+        this.palabrasReservadas.put("instancia", TipoToken.INSTANCEOF);
+        this.palabrasReservadas.put("nuevo", TipoToken.NEW);
+        this.palabrasReservadas.put("verdadero", TipoToken.TRUE);
+        this.palabrasReservadas.put("falso", TipoToken.FALSE);
+        this.palabrasReservadas.put("vacio", TipoToken.NULL);
     }
-
-    Scanner(String source){
-        this.source = source;
+    
+    private int nextChar() throws IOException {
+        return currentChar = input.read();
     }
-
-    List<Token> scanTokens(){
-        //Aquí va el corazón del scanner.
-
-        /*
-        Analizar el texto de entrada para extraer todos los tokens
-        y al final agregar el token de fin de archivo
-         */
-        tokens.add(new Token(TipoToken.EOF, "", null, linea));
-
-        return tokens;
+    
+    private boolean isLetter(int c) {
+        return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
     }
+    
+    private boolean isDigit(int c) {
+        return c >= '0' && c <= '9';
+    }
+    
+    public Token getNextToken() throws IOException {
+        int estado = 0;
+        String lexema = "";
+        
+        while (currentChar != -1) {
+            switch (estado) {
+                case 0:
+                    if (isLetter(currentChar)) {
+                        estado = 1;
+                        lexema += (char)currentChar;
+                        nextChar();
+                    } else if (isDigit(currentChar)) {
+                        estado = 2;
+                        lexema += (char)currentChar;
+                        nextChar();
+                    } else if (currentChar == '+') {
+                        estado = 3;
+                        lexema += (char)currentChar;
+                        nextChar();
+                        return new Token(TipoToken.SUMA, lexema);
+                    } else if (currentChar == '-') {
+                        estado = 3;
+                        lexema += (char)currentChar;
+                        nextChar();
+                        return new Token(TipoToken.RESTA, lexema);
+                    } else if (currentChar == ';') {
+                        estado = 3;
+                        lexema += (char)currentChar;
+                        nextChar();
+                        return new Token(TipoToken.PUNTO_Y_COMA, lexema);
+
+                     } else if (currentChar == '.') {
+                        estado = 3;
+                        lexema += (char)currentChar;
+                        nextChar();
+                        return new Token(TipoToken.PUNTO, lexema);
+                     } else if (currentChar == ':') {
+                        estado = 3;
+                        lexema += (char)currentChar;
+                        nextChar();
+                        return new Token(TipoToken.DOS_PUNTOS, lexema);
+                    } else if (currentChar == ',') {
+                        estado = 3;
+                        lexema += (char)currentChar;
+                        nextChar();
+                        return new Token(TipoToken.COMA, lexema);
+                    } else if (currentChar == '?') {
+                        estado = 3;
+                        lexema += (char)currentChar;
+                        nextChar();
+                        return new Token(TipoToken.SIGNO_DE_INTERROGACION, lexema);    
+                    } else if (currentChar == '!') {
+                        estado = 3;
+                        lexema += (char)currentChar;
+                        nextChar();
+                        return new Token(TipoToken.SIGNO_DE_EXCLAMACION, lexema);
+                    } else if (currentChar == '[') {
+                        estado = 3;
+                        lexema += (char)currentChar;
+                        nextChar();
+                        return new Token(TipoToken.COCHETE_IZQ, lexema);
+                    } else if (currentChar == ']') {
+                        estado = 3;
+                        lexema += (char)currentChar;
+                        nextChar();
+                        return new Token(TipoToken.COCHETE_IZQ, lexema);
+                    } else if (currentChar == '{') {
+                        estado = 3;
+                        lexema += (char)currentChar;
+                        nextChar();
+                        return new Token(TipoToken.LLAVE_IZQ, lexema);
+                    } else if (currentChar == '}') {
+                        estado = 3;
+                        lexema += (char)currentChar;
+                        nextChar();
+                        return new Token(TipoToken.LLAVE_DER, lexema);
+                    } else if (currentChar == '_') {
+                        estado = 3;
+                        lexema += (char)currentChar;
+                        nextChar();
+                        return new Token(TipoToken.GUION_BAJO, lexema);
+                    } else if (currentChar == '¨') {
+                        estado = 3;
+                        lexema += (char)currentChar;
+                        nextChar();
+                        return new Token(TipoToken.DIERESIS, lexema);
+                    } else if (currentChar == '/') {
+                        estado = 3;
+                        lexema += (char)currentChar;
+                        nextChar();
+                        return new Token(TipoToken.DIAGONAL, lexema);
+                    } else if (currentChar == '*') {
+                        estado = 3;
+                        lexema += (char)currentChar;
+                        nextChar();
+                        return new Token(TipoToken.MULTIPLICACION, lexema);
+                    } else if (currentChar == '#') {
+                        estado = 3;
+                        lexema += (char)currentChar;
+                        nextChar();
+                        return new Token(TipoToken.NUMERAL, lexema);
+                    } else if (currentChar == '&') {
+                        estado = 3;
+                        lexema += (char)currentChar;
+                        nextChar();
+                        return new Token(TipoToken.AMPERSAND, lexema);
+                    } else if (currentChar == '%') {
+                        estado = 3;
+                        lexema += (char)currentChar;
+                        nextChar();
+                        return new Token(TipoToken.PORCENTAJE, lexema);
+                    } else if (currentChar == '>') {
+                        estado = 3;
+                        lexema += (char)currentChar;
+                        nextChar();
+                        return new Token(TipoToken.MAYORQUE, lexema);
+                    } else if (currentChar == '<') {
+                        estado = 3;
+                        lexema += (char)currentChar;
+                        nextChar();
+                        return new Token(TipoToken.MENORQUE, lexema);
+                    } else if (currentChar == '>=') {
+                        estado = 3;
+                        lexema += (char)currentChar;
+                        nextChar();
+                        return new Token(TipoToken.MAYOR_IGUAL, lexema);
+                    } else if (currentChar == '<=') {
+                        estado = 3;
+                        lexema += (char)currentChar;
+                        nextChar();
+                        return new Token(TipoToken.MENOR_IGUAL, lexema);
+                    } else if (currentChar == '=') {
+                        estado = 3;
+                        lexema += (char)currentChar;
+                        nextChar();
+                        return new Token(TipoToken.IGUAL, lexema);
+                    } else if (currentChar == '""') {
+                        estado = 3;
+                        lexema += (char)currentChar;
+                        nextChar();
+                        return new Token(TipoToken.COMILLAS_DOB, lexema);
+                    } else if (currentChar == '''') {
+                        estado = 3;
+                        lexema += (char)currentChar;
+                        nextChar();
+                        return new Token(TipoToken.COMILLAS_SIMP, lexema);
+                    } else if (currentChar == '|') {
+                        estado = 3;
+                        lexema += (char)currentChar;
+                        nextChar();
+                        return new Token(TipoToken.PIPA, lexema);
+                    } else if (currentChar == ' ') {
+                        nextChar();
+                    } else {
+                        throw new RuntimeException("Caracter no reconocido: " + (char)currentChar);
+                    }
+                    break;
+                    
+                    
+                case 1:
+                    if (isLetter(currentChar) || isDigit(currentChar)) {
+                        lexema += (char)currentChar;
+                        nextChar();
+                    } else {
+                        estado = 3;
+                        TipoToken tipo = palabrasReservadas.get(lexema.toUpperCase());
+                        if (tipo == null) {
+                            tipo = TipoToken.IDENTIFICADOR;
+                        }
+                        return new Token(tipo, lexema);
+                    }
+                    break;
+                    
+                case 2:
+                    if (isDigit(currentChar)) {
+                        lexema += (char)currentChar;
+                        nextChar();
+                    } else {
+                        estado = 3;
+                        return new Token(TipoToken.NUMERO, lexema);
+                    }
+                    break;
+
+                case 3:
+                    if (isDigit(currentChar)) {
+                        lexema += (char)currentChar;
+                        nextChar();
+                    } else {
+                        estado = 3;
+                        return new Token(TipoToken.LETRA, lexema);
+                    }
+                    break;
+
+                default:
+                    throw new RuntimeException("Estado no válido: " + estado);
+            }
+        }
+        
+        return new Token(TipoToken.EOF, "");
+    }
+    
 }
-
-/*
-Signos o símbolos del lenguaje:
-(
-)
-{
-}
-,
-.
-;
--
-+
-*
-/
-!
-!=
-=
-==
-<
-<=
->
->=
-// -> comentarios (no se genera token)
-/* ... * / -> comentarios (no se genera token)
-Identificador,
-Cadena
-Numero
-Cada palabra reservada tiene su nombre de token
-
- */
+    
